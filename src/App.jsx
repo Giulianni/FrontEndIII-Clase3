cddb53cf6eb1a41349bcb5a795f3a6832bfadabf
GGiulianni <nicoligiu2@gmai.com>
2021-08-12 21:05:04 -0300

    practica integradora 1

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import './App.css';
+import Button from "./components/Button";
+import Listado from './components/Listado';
+
+function App() {
+  //Necesarias tantas variables??
+  let arregloDeFrases  = ["Haz Click", "Click si eres valiente", "Prueba otra frase", "Yo controlo el boton"];
+
+// Funcion que obtiene una frase del boton, espera un arreglo... REFACTORIZAR 
+  function fraseRandom(fraseActual) {
+    let indice = Math.floor(Math.random() * 4); // Numero random
+
+    while (fraseActual === arregloDeFrases[indice]) {
+      indice = Math.floor(Math.random() * 4);
+    }
+    fetchData();
+    return arregloDeFrases[indice]
+  }
+
+  const [fraseChuck, setFraseChuck] = useState("") // Manera de react para crear estados
+
+  async function fetchData() {
+    const resultado = await axios.get('https://api.chucknorris.io/jokes/random')
+    setFraseChuck(resultado.data.value) // Mandera de react para actualizar estados
+  };
+
+  return (
+    <div className="app">
+      <Button
+        defaultFrase={arregloDeFrases[0]}
+        onClick={fraseRandom}
+        fraseChuck={fraseChuck}
+      />
+      <Listado listadoDeFrases={fraseChuck} />
+    </div>
+
+  )
+
+}
+
+export default App;
